ccce4bd82331458867096930f39e9a517a356b7e
el-milly <elijahhome.job@gmail.com>
2025-07-01 14:33:21 +0300

    Category Serializer

---
+++ product_app/serializer.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import CategoryModel
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ['name', 'slug']
+        model = CategoryModel
+
+    def create(self, validated_data):
+        # Check if category with this name already exists
+        if CategoryModel.objects.filter(name=validated_data['name']).exists():
+            raise serializers.ValidationError("A category with this name already exists.")
+        
+        # Create and return the new category
+        return CategoryModel.objects.create(**validated_data)
