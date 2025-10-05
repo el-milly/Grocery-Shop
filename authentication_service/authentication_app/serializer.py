d9978093b26f6733894a4d17e96e565b270864b2
el-milly <elijahhome.job@gmail.com>
2025-10-05 22:01:27 +0300

    Authentication serializer, views update

--- authentication_service/authentication_app/serializer.py
+++ authentication_service/authentication_app/serializer.py
@@ -6,14 +6,14 @@ import json
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields= ['username', 'password']
+        fields= ['id','username', 'password']
         extra_kwargs = {'password': {'write_only': True}}
 
 class UserProfileSerializer(serializers.ModelSerializer):
     user = UserSerializer()
     class Meta:
         model = user_profile
-        fields = ['first_name', 'email', 'user', 'last_name']
+        fields = ['id','first_name', 'email', 'user', 'last_name']
 
     def create(self, validated_data):
         user_data = validated_data.pop('user')
